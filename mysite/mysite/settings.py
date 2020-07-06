f28788fdb914d1487396e945e65209541f53ed7c
Sumit Kumar Mallick <mallicksumit546@gmail.com>
2020-07-06 18:35:53 +0530

    Secret key added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = my_app_SECRET_KEY
+SECRET_KEY = 'cdme%ec+!=96m(t3&y96_)!@)@hi&ka&10xihbxmrh+2=#kvex'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sumit-api-project.herokuapp.com']
+ALLOWED_HOSTS = ['sumit-api-project.herokuapp.com', '127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
